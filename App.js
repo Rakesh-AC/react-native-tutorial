c9fb05e70ad49ffbddb4476a419a927374eea8a3
Rakesh-AC <rakesh.ac@parachutetech.ai>
2024-04-13 17:23:11 +0530

    commting

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import Profile  from './components/profile';
 export default function App() {
+  const [name, setName] = useState('');
+
+  const handleChangeText = (text) => {
+    setName(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello, {name}!</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={handleChangeText}
+        value={name}
+        placeholder="Enter your name"
+      />
+      <Button title="Greet Me" onPress={() => console.log(`Hello, ${name}!`)} />
+      {/* <StatusBar style="auto" /> */}
+
+      <Profile></Profile>
     </View>
   );
 }
@@ -16,5 +31,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
